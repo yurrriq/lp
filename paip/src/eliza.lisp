62c1bc6a66ca04469db281f49fa5bd14bb0b9f07
Eric Bailey <eric@ericb.me>
2017-09-13 21:11:28 -0500

    paip: progress

--- paip/src/eliza.lisp
+++ paip/src/eliza.lisp
@@ -9,22 +9,54 @@
 (defconstant no-bindings '((t . t))
   "Indicates pat-match success, with no variables.")
 
-(defun starts-with (list x)
-  "Is this a list whose first element is x?"
-  (and (consp list) (eql (first list) x)))
+(defun eliza ()
+  "Respond to user input using pattern matching rules."
+  (loop
+    (print 'eliza>)
+    (write (flatten (use-eliza-rules (read))) :pretty t)))
 
-(defun simple-equal (x y)
-  "Are x and y equal? (Don't check inside strings.)"
-  (if (or (atom x) (atom y))
-      (eql x y)
-      (and (simple-equal (first x) (first y))
-           (simple-equal (rest x) (rest y)))))
+(defparameter *eliza-rules*
+  '((((?* ?x) hello (?* ?y))
+     (How do you do.  Please state your problem.))
+    (((?* ?x) I want (?* ?y))
+     (What would it mean if you got ?y)
+     (Why do you want ?y) (Suppose you got ?y soon))
+    (((?* ?x) if (?* ?y))
+     (Do you really think its likely that ?y) (Do you wish that ?y)
+     (What do you think about ?y) (Really-- if ?y))
+    (((?* ?x) no (?* ?y))
+     (Why not?) (You are being a bit negative)
+     (Are you saying "NO" just to be negative?))
+    (((?* ?x) I was (?* ?y))
+     (Were you really?) (Perhaps I already knew you were ?y)
+     (Why do you tell me you were ?y now?))
+    (((?* ?x) I feel (?* ?y))
+     (Do you often feel ?y ?))
+    (((?* ?x) I felt (?* ?y))
+     (What other feelings do you have?))))
+
+(defun rule-pattern (rule) (first rule))
+(defun rule-responses (rule) (rest rule))
+
+(defun use-eliza-rules (input)
+  "Find some rule with which to transform the input."
+  (some #'(lambda (rule)
+            (let ((result (pat-match (rule-pattern rule) input)))
+              (unless (eq result fail)
+                (sublis (switch-viewpoint result)
+                        (random-elt (rule-responses rule))))))
+        *eliza-rules*))
+
+(defun switch-viewpoint (words)
+  "Change I to you and vice versa, and so on."
+  (sublis '((I . you) (you . I) (me . you) (am . are))
+          words))
 
 (defun pat-match (pattern input &optional (bindings no-bindings))
   "Match pattern against input in the context of the bindings."
-  (cond ((eq bindings fail)          fail)
-        ((variable-p pattern)        (match-variable pattern input bindings))
-        ((eql pattern input)         bindings)
+  (cond ((eq bindings fail) fail)
+        ((variable-p pattern) (match-variable pattern input bindings))
+        ((eql pattern input) bindings)
         ((segment-pattern-p pattern) (segment-match pattern input bindings))
         ((and (consp pattern) (consp input))
          (pat-match (rest pattern) (rest input)
@@ -39,11 +71,6 @@
           ((equal input (binding-val binding)) bindings)
           (t fail))))
 
-(defun segment-pattern-p (pattern)
-  "Is this a segment matching pattern: ((?* var) . pat)"
-  (and (consp pattern)
-       (starts-with (first pattern) '?*)))
-
 (defun segment-match (pattern input bindings &optional (start 0))
   "Match the segment pattern ((?* var) . pat) against input."
   (let ((var (second (first pattern)))
@@ -54,12 +81,15 @@
                              :start start :test #'equal)))
           (if (null pos)
               fail
-              (let ((b2 (pat-match pat (subseq input pos)
-                                   (match-variable var (subseq input 0 pos)
-                                                   bindings))))
+              (let ((b2 (pat-match pat (subseq input pos) bindings)))
                 (if (eq b2 fail)
                     (segment-match pattern input bindings (+ pos 1))
-                    b2)))))))
+                    (match-variable var (subseq input 0 pos) b2))))))))
+
+(defun segment-pattern-p (pattern)
+  "Is this a segment matching pattern: ((?* var) . pat)"
+  (and (consp pattern)
+       (starts-with (first pattern) '?*)))
 
 (defun variable-p (x)
   "Is x a variable (a symbol beginning with '?')?"
@@ -84,3 +114,25 @@
         (if (eq bindings no-bindings)
             nil
             bindings)))
+
+(defun flatten (the-list)
+  "Append together elements (or lists) in the list."
+  (mappend #'mklist the-list))
+
+(defun mklist (x)
+  "Return x if it is a list, otherwise (x)."
+  (if (listp x)
+      x
+      (list x)))
+
+(defun random-elt (choices)
+  "Choose an element from a list at random."
+  (elt choices (random (length choices))))
+
+(defun mappend (fn the-list)
+  "Apply fn to each element of list and append the results."
+  (apply #'append (mapcar fn the-list)))
+
+(defun starts-with (list x)
+  "Is this a list whose first element is x?"
+  (and (consp list) (eql (first list) x)))
